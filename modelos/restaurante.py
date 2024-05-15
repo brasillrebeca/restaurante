46253abaaeda66e15f966f829e11e0ded5c131eb
brasillrebeca <rebecabrasil9450@gmail.com>
2024-05-15 15:34:06 -0300

    inserindo avaliações

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,51 +1,54 @@
 # Cada objeto encapsula dados e comportamentos relacionados
 # objeto:instância de uma classe - criar objeto: instanciar um objeto
 # a classe acompanha atributos e características
-'''
-class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
-'''
+
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     #variável lsita
     restaurantes = []
 
     # inicializando atribuições direcionadas para cada elemento novo criado com a definição do método init + referência self + valores a serem recebidos
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria
         # atributo privado _
         self._ativo = False
+        self._avaliacao = []
         # ao criar um objeto, fazer ele entrar na variável lista
         Restaurante.restaurantes.append(self)
     
     # retornar a informação sobre o objeto em formato de string, invés de em formato de endereço na memória
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     # criando meus próprios métodos - função para exibir informações dos restaurantes armazenados
-    def listar_restaurantes():
-        print(f'{'Nome do restaurante'} | {'Categoria'} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    # método da classa @classmethod - não precisa instanciar o objeto
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} |{'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
 
-# modificando como um atributo vai ser lido através do @property
-@property
-def ativo(self):
-    return 'verddeiro' if self._ativo else 'falso'
+    # modificando como um atributo vai ser lido através do @property
+    @property
+    def ativo(self):
+        return 'ativo' if self._ativo else 'inativo'
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-# usar . para acessar atributos do objeto
-'''restaurante_praca.nome = 'Praça'''''
-restaurante_pizza = Restaurante('Pizza express', 'Italiano')
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-# o dir é utilizado para exibir todos os métodos do objeto
-# o vars é utilizado para exibir apenas os métodos especificados através do código
-'''
-print(dir(restaurante_praca))
-print(vars(restaurante_pizza))
-'''
+# método que recebe as avaliações
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
 
-# método próprio de classes: listar_restaurantes()
-Restaurante.listar_restaurantes()
+# método para exibir a média da avaliação
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
